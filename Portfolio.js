6a9d672a9b89006b5547a8d711add249943eb77c
komalgurni23 <komalgurnani2003@gmail.com>
2026-06-18 17:35:33 +0530

    Add files via upload

---
+++ Portfolio.js
@@ -0,0 +1,32 @@
+// Smooth Fade Animation
+
+const sections = document.querySelectorAll(".section");
+
+window.addEventListener("scroll", () => {
+
+    sections.forEach(section => {
+
+        const position =
+        section.getBoundingClientRect().top;
+
+        if(position < window.innerHeight - 100){
+
+            section.style.opacity = "1";
+            section.style.transform =
+            "translateY(0)";
+        }
+    });
+});
+
+// Initial State
+
+sections.forEach(section => {
+
+    section.style.opacity = "0";
+
+    section.style.transform =
+    "translateY(50px)";
+
+    section.style.transition =
+    "all 0.8s ease";
+});
